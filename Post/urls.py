d562b908305d81333c1db283809713a9d6c675fe
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-10 23:25:43 +0600

    UpdateView

--- Post/urls.py
+++ Post/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name="post_edit"),
     path("", BlogListView.as_view(), name="home"),
 ]
